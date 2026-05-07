8b0dd59bd2a46f458149827fb23c98daed624148
Eya BL <eyabenltaief6@gmail.com>
2026-05-07 21:17:30 +0100

    tp2

---
+++ playwright.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig, devices } from '@playwright/test';
+
+export default defineConfig({
+  testDir: './tests',
+
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+    {
+      name: 'edge',
+      use: { ...devices['Desktop Edge'] },
+    },
+  ],
+});
